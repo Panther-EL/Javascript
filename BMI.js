b736f1a533b4b4fc6232869892198d457b283e56
Panther-EL <agbekoelorm32@gmail.com>
2025-09-21 22:08:43 +0000

    Create BMI

---
+++ BMI.js
@@ -0,0 +1,13 @@
+const BMI = (weight,height) => {
+let BMI = weight/(height*height);
+
+if(BMI<18.5){
+  return 'Underweight';
+}else if(BMI>18.5 && BMI<24.9){
+  return 'Normal';
+}else{
+  return 'Overweight';
+}
+};
+
+console.log(BMI())
