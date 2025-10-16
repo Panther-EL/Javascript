a468ba117e06324b56204cdae123edaf6390499b
Panther-EL <agbekoelorm32@gmail.com>
2025-10-16 21:36:51 +0000

    Add task reading and writing functions

---
+++ Taskmanager/Filehandler.js
@@ -0,0 +1,8 @@
+//Handles reading and writing tasks.json
+export const readTask = () => {
+    console.log('Read tasks module loaded')
+}
+
+export const writeTask = () => {
+    console.log('Write tasks module loaded')
+}
