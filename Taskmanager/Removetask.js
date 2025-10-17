90e181b441e0e11e28d3df49c164107041813c4b
Panther-EL <agbekoelorm32@gmail.com>
2025-10-17 21:43:29 +0000

    Implement task removal functionality

--- Taskmanager/Removetask.js
+++ Taskmanager/Removetask.js
@@ -1,4 +1,18 @@
 //Handles removing a task
-export const removeTask = () => {
+
+import { readTask,writeTask } from "../utils/fileHandler.js"
+
+export const removeTask = (id) => {
     console.log('Remove task module loaded')
+    const tasks = readTask() //Read all tasks from the JSON file
+    const updatedTasks = tasks.filter(task => task.id !== id) //Filtering out the tasks that matches the given id
+    if(updatedTasks.length === tasks.length){
+        console.log('There is no task with such ID')
+        return
+    }
+    writeTask(updatedTasks)
+    console.log('Task removed successfully')
 }
+
+
+
