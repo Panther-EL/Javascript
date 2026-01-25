e2b910674f9a1532d4e63c0175cc3595859e84cd
Panta <agbekoelorm32@gmail.com>
2026-01-25 23:47:44 +0000

    Delete Taskmanager/Filehandler.js

--- Taskmanager/Filehandler.js
+++
@@ -1,8 +0,0 @@
-//Handles reading and writing tasks.json
-export const readTask = () => {
-    console.log('Read tasks module loaded')
-}
-
-export const writeTask = () => {
-    console.log('Write tasks module loaded')
-}
